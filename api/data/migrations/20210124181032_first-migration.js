938d8be6caa9590d3266afadc1dbd96a7c319b6d
ashleyannlaz <paperpastels@gmail.com>
2021-09-19 23:18:47 -0400

    Created Migration

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,46 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("user_name").notNullable();
+      users.string("user_username", 200).notNullable().unique();
+      users.string("user_password", 200).notNullable();
+      users.boolean("role").defaultTo(0);
     })
-}
+
+    .createTable("classes", (classes) => {
+      classes.increments("class_id");
+      classes.string("class_name");
+      classes.string("class_type");
+      classes.string("class_duration");
+      classes.string("class_intensity");
+      classes.string("class_location");
+      classes.string("class_max");
+    })
+
+    .createTable("class_attendees", (attendees) => {
+      attendees.increments("attendees_id");
+      attendees
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      attendees
+        .integer("class_id")
+        .unsigned()
+        .notNullable()
+        .references("class_id")
+        .inTable("classes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists("class_attendees");
+  await knex.schema.dropTableIfExists("classes");
+  await knex.schema.dropTableIfExists("users");
+};
